18c03ec2cd6133656cbb0f6fb0ecc8bcacce4e5d
Dan Nightingale <dan.nightingale@gmail.com>
2026-08-23 16:12:53 +0100

    Revert PulseAudio workarounds in restart_services.sh

--- scripts/restart_services.sh
+++ scripts/restart_services.sh
@@ -5,31 +5,12 @@ set -x
 my_dir=$HOME/BirdNET-Pi/scripts
 
 
-sudo systemctl stop birdnet_recording.service livestream.service
-
-# Identify the BirdNET user
-TARGET_USER="${BIRDNET_USER}"
-if [ -z "${TARGET_USER}" ]; then
-  TARGET_USER=$(awk -F: '/1000/ {print $1}' /etc/passwd)
-fi
-
-# Terminate existing PulseAudio instances to prevent duplicates/locks
-if [ -n "${TARGET_USER}" ]; then
-  sudo -u "${TARGET_USER}" pulseaudio -k 2>/dev/null || true
-  sleep 1
-  if pgrep -u "${TARGET_USER}" pulseaudio &>/dev/null; then
-    sudo pkill -9 -u "${TARGET_USER}" pulseaudio 2>/dev/null || true
-  fi
-  # Start a fresh PulseAudio instance and let it initialize the ALSA soundcard first
-  sudo -u "${TARGET_USER}" pulseaudio --start --exit-idle-time=-1 2>/dev/null || true
-  sleep 3
-fi
+sudo systemctl stop birdnet_recording.service
 
 services=(chart_viewer.service
   spectrogram_viewer.service
   icecast2.service
   birdnet_recording.service
-  livestream.service
   birdnet_analysis.service
   birdnet_log.service
   birdnet_stats.service)
@@ -39,20 +20,10 @@ for i in  "${services[@]}";do
 done
 
 for i in {1..5}; do
-  # We want to loop here (5*5seconds) until the critical services are running
-  if systemctl is-active --quiet birdnet_recording.service && \
-     systemctl is-active --quiet livestream.service && \
-     systemctl is-active --quiet birdnet_analysis.service; then
-      logger "[$0] Critical audio and analysis services are running"
-      break
-  fi
+  # We want to loop here (5*5seconds) until the birdnet_analysis.service is running
+  systemctl is-active --quiet birdnet_analysis.service \
+	  && logger "[$0] birdnet_analysis.service is running" \
+	  && break
 
   sleep 5
-done
-
-# Verify vital services and report error if unable to return system to working state
-for service in birdnet_recording.service livestream.service birdnet_analysis.service; do
-  if ! systemctl is-active --quiet "$service"; then
-    logger -s -p user.err "[$0] ERROR: $service failed to start or is inactive after restart!"
-  fi
 done
